03beb17de895b34369224adf2e96bdcc1bb36d9d
guaju <guaju@126.com>
2018-08-07 23:53:52 +0800

    Initial commit

---
+++ app/src/main/java/com/example/jingbin/cloudreader/ui/menu/LoginActivity.java
@@ -0,0 +1,66 @@
+package com.example.jingbin.cloudreader.ui.menu;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.jingbin.cloudreader.R;
+import com.example.jingbin.cloudreader.base.BaseActivity;
+import com.example.jingbin.cloudreader.databinding.ActivityLoginBinding;
+import com.example.jingbin.cloudreader.viewmodel.menu.LoginNavigator;
+import com.example.jingbin.cloudreader.viewmodel.menu.LoginViewModel;
+
+import rx.Subscription;
+
+/**
+ * @author jingbin
+ */
+public class LoginActivity extends BaseActivity<ActivityLoginBinding> implements LoginNavigator {
+
+    private LoginViewModel viewModel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        setTitle("玩安卓登录");
+        showContentView();
+
+        viewModel = new LoginViewModel();
+        viewModel.setNavigator(this);
+        bindingView.setViewmodel(viewModel);
+    }
+
+    public void register(View view) {
+        viewModel.register();
+    }
+
+    public void login(View view) {
+        viewModel.login();
+    }
+
+    /**
+     * 注册或登录成功
+     */
+    @Override
+    public void loadSuccess() {
+        finish();
+    }
+
+    @Override
+    public void addRxSubscription(Subscription subscription) {
+        addSubscription(subscription);
+    }
+
+    public static void start(Context mContext) {
+        Intent intent = new Intent(mContext, LoginActivity.class);
+        mContext.startActivity(intent);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        viewModel.onDestroy();
+    }
+}
